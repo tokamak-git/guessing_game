2adab1306b6c9d68a024186c44bf27f968667be5
tokamak <gaurav@raghuvanshy.com>
2024-01-21 20:19:27 +0100

    Added randomness
    Added random number, updated guessing mechanism,
    Created loop for 3 tries.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 // add imports for this program
-// use rand::Rng;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("You have 3 chances to guess the number.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..11);
+    let mut counter = 1;
+    loop {
+        let mut guess = String::new();
+        println!("The secret number is: {}", secret_number);
+        println!("Please input your guess.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
-    // guess();
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        if counter == 3 {
+            println!("You lose!");
+            break;
+        }
+
+        println!("You guessed: {}", guess);
+        counter += 1;
+    }
 }
 
 // fn guess() {
